62908a9de4bc91d3f7ba47b94244454345ff2147
kafri8889 <kafri8889@gmail.com>
2023-06-18 21:13:23 +0700

    Add `Headers` annotation to `getBalance` function in `BalanceService`

--- app/src/main/java/com/dcns/dailycost/data/datasource/remote/services/BalanceService.kt
+++ app/src/main/java/com/dcns/dailycost/data/datasource/remote/services/BalanceService.kt
@@ -4,10 +4,12 @@ import com.dcns.dailycost.data.model.networking.response.BalanceResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.Path
 
 interface BalanceService {
 
+    @Headers("Content-Type: application/json;charset=UTF-8")
     @GET("/api/saldo/{id}")
     suspend fun getBalance(
         @Path("id") userId: Int,
