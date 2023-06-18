804b05906cb44fca8cbed0f2c8bfedba23ad64ec
kafri8889 <kafri8889@gmail.com>
2023-06-18 15:36:00 +0700

    Add new param `status` to `ErrorResponse`

--- app/src/main/java/com/dcns/dailycost/data/model/networking/response/ErrorResponse.kt
+++ app/src/main/java/com/dcns/dailycost/data/model/networking/response/ErrorResponse.kt
@@ -3,5 +3,6 @@ package com.dcns.dailycost.data.model.networking.response
 import com.dcns.dailycost.foundation.common.IResponse
 
 data class ErrorResponse(
-    val message: String
+    val message: String,
+    val status: String
 ): IResponse
