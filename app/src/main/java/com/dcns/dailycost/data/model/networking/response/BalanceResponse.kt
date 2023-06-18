9ab7ad7e41705ded3d211cbe300a21c6475c56ed
kafri8889 <kafri8889@gmail.com>
2023-06-18 21:17:01 +0700

    Extend `BalanceResponse` with `IResponse`

--- app/src/main/java/com/dcns/dailycost/data/model/networking/response/BalanceResponse.kt
+++ app/src/main/java/com/dcns/dailycost/data/model/networking/response/BalanceResponse.kt
@@ -1,8 +1,9 @@
 package com.dcns.dailycost.data.model.networking.response
 
 import com.dcns.dailycost.data.model.networking.BalanceResponseData
+import com.dcns.dailycost.foundation.common.IResponse
 
 data class BalanceResponse(
     val data: BalanceResponseData,
     val status: String
-)
+): IResponse
