aa993655139c94ae1ab23c37365198d50e8ffc55
kafri8889 <kafri8889@gmail.com>
2023-06-20 16:27:49 +0700

    Create `SelectableWalletItem`

--- app/src/main/java/com/dcns/dailycost/foundation/uicomponent/WalletItem.kt
+++ app/src/main/java/com/dcns/dailycost/foundation/uicomponent/WalletItem.kt
@@ -1,5 +1,9 @@
 package com.dcns.dailycost.foundation.uicomponent
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Arrangement
@@ -8,10 +12,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,11 +33,75 @@ import com.dcns.dailycost.foundation.common.LocalCurrency
 import com.dcns.dailycost.foundation.common.primarySystemLocale
 import com.dcns.dailycost.foundation.extension.drawFadedEdge
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SelectableWalletItem(
+    wallet: Wallet,
+    selected: Boolean,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+
+    val scaleAnimatable = remember { Animatable(1f) }
+
+    LaunchedEffect(selected) {
+        if (selected) {
+            scaleAnimatable.animateTo(
+                targetValue = 0.88f,
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy
+                )
+            )
+
+            scaleAnimatable.animateTo(
+                targetValue = 1f,
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy
+                )
+            )
+        }
+    }
+
+    Card(
+        onClick = onClick,
+        border = if (selected) BorderStroke(
+            width = 1.dp,
+            color = MaterialTheme.colorScheme.outline
+        ) else null,
+        modifier = modifier
+            .graphicsLayer {
+                scaleX = scaleAnimatable.value
+                scaleY = scaleAnimatable.value
+            }
+    ) {
+        WalletItemContent(
+            wallet = wallet,
+            trailingIcon = {
+                RadioButton(
+                    selected = selected,
+                    onClick = onClick
+                )
+            }
+        )
+    }
+}
+
 @Composable
 fun WalletItem(
     wallet: Wallet,
     modifier: Modifier = Modifier
+) {
+
+    Card(modifier = modifier) {
+        WalletItemContent(wallet = wallet)
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun WalletItemContent(
+    wallet: Wallet,
+    trailingIcon: @Composable () -> Unit = {}
 ) {
 
     val currency = LocalCurrency.current
@@ -43,48 +114,50 @@ fun WalletItem(
         )
     }
 
-    Card(
-        modifier = modifier
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
+        Icon(
+            painter = painterResource(wallet.walletType.icon),
+            contentDescription = null
+        )
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
+                .weight(1f)
         ) {
-            Icon(
-                painter = painterResource(wallet.walletType.icon),
-                contentDescription = null
+            Text(
+                text = wallet.walletType.localizedName,
+                style = MaterialTheme.typography.bodySmall
             )
 
-            Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-                Text(
-                    text = wallet.walletType.localizedName,
-                    style = MaterialTheme.typography.bodySmall
-                )
-
-                AnimatedTextByChar(
-                    text = balance,
-                    style = MaterialTheme.typography.titleMedium,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        // Rendering to an offscreen buffer is required to get the faded edges' alpha to be
-                        // applied only to the text, and not whatever is drawn below this composable (e.g. the
-                        // window).
-                        .graphicsLayer { compositingStrategy = CompositingStrategy.Offscreen }
-                        .drawWithContent {
-                            drawContent()
-                            drawFadedEdge(
-                                edgeWidth = 8.dp,
-                                leftEdge = false
-                            )
-                        }
-                        .basicMarquee(
-                            delayMillis = 2000
+            AnimatedTextByChar(
+                text = balance,
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    // Rendering to an offscreen buffer is required to get the faded edges' alpha to be
+                    // applied only to the text, and not whatever is drawn below this composable (e.g. the
+                    // window).
+                    .graphicsLayer { compositingStrategy = CompositingStrategy.Offscreen }
+                    .drawWithContent {
+                        drawContent()
+                        drawFadedEdge(
+                            edgeWidth = 8.dp,
+                            leftEdge = false
                         )
-                )
-            }
+                    }
+                    .basicMarquee(
+                        delayMillis = 2000
+                    )
+            )
         }
+
+        trailingIcon()
     }
 }
