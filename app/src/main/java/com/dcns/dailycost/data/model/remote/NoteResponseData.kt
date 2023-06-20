f1f9a0d46992929f58c802df93757e27fc4fe111
kafri8889 <kafri8889@gmail.com>
2023-06-20 11:13:41 +0700

    Add new param `createdAtEpoch` to `NoteResponseData`

--- app/src/main/java/com/dcns/dailycost/data/model/remote/NoteResponseData.kt
+++ app/src/main/java/com/dcns/dailycost/data/model/remote/NoteResponseData.kt
@@ -5,6 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class NoteResponseData(
     @SerializedName("body") val body: String,
     @SerializedName("created_at") val createdAt: String,
+    @SerializedName("created_at_epoch") val createdAtEpoch: Long,
     @SerializedName("catatan_id") val id: Int,
     @SerializedName("url") val imageUrl: String,
     @SerializedName("title") val title: String,
--- app/src/main/java/com/dcns/dailycost/foundation/extension/NoteResponseDataExt.kt
+++ app/src/main/java/com/dcns/dailycost/foundation/extension/NoteResponseDataExt.kt
@@ -2,16 +2,12 @@ package com.dcns.dailycost.foundation.extension
 
 import com.dcns.dailycost.data.model.Note
 import com.dcns.dailycost.data.model.remote.NoteResponseData
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 fun NoteResponseData.toNote(): Note {
-    val dateFormatter = SimpleDateFormat("dd MMMM yyyy", Locale.US)
-
     return Note(
         id = id.toString(),
         body = body,
-        createdAt = dateFormatter.parse(createdAt)?.time ?: System.currentTimeMillis(),
+        createdAt = createdAtEpoch,
         imageUrl = imageUrl,
         title = title,
         userId = userId.toString(),
