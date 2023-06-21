69ae42dcaaa6a85cb4aa019e832cb7359da2fe3e
kafri8889 <kafri8889@gmail.com>
2023-06-21 21:40:29 +0700

    NotificationUtil

---
+++ app/src/main/java/com/dcns/dailycost/foundation/util/NotificationUtil.kt
@@ -0,0 +1,23 @@
+package com.dcns.dailycost.foundation.util
+
+object NotificationUtil {
+
+//    const val WORKER_CHANNEL_ID = "worker"
+//    const val WORKER_CHANNEL_NAME = "Sync worker"
+//
+//    private val notificationManager: (Context) -> NotificationManager = {
+//        it.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//    }
+//
+//    @RequiresApi(Build.VERSION_CODES.O)
+//    fun createChannel(context: Context) {
+//        val workerChannel = NotificationChannel(
+//            WORKER_CHANNEL_ID,
+//            WORKER_CHANNEL_NAME,
+//            NotificationManager.IMPORTANCE_HIGH
+//        )
+//
+//        notificationManager(context).createNotificationChannel(workerChannel)
+//    }
+
+}
