47036a6418d73a316bee3d643f5994631e2d6b28
kafri8889 <kafri8889@gmail.com>
2023-06-26 13:52:37 +0700

    [RetrofitRequestBody] Remove unused function

--- app/src/main/java/com/dcns/dailycost/foundation/common/RetrofitRequestBody.kt
+++ app/src/main/java/com/dcns/dailycost/foundation/common/RetrofitRequestBody.kt
@@ -1,7 +1,6 @@
 package com.dcns.dailycost.foundation.common
 
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONObject
@@ -9,12 +8,6 @@ import org.json.JSONObject
 abstract class RetrofitRequestBody: Jsonable() {
 
     abstract fun getBody(): Map<String, Any>
-
-    fun appendToMultipartBody(builder: MultipartBody.Builder) {
-        getBody().forEach { (k, v) ->
-            builder.addFormDataPart(k, v.toString())
-        }
-    }
 
     fun toRequestBody(): RequestBody {
         return JSONObject(getBody())
