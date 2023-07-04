8eade78e2bd9726cd0d7c9a2a71cc7e89fb8f372
kafri8889 <kafri8889@gmail.com>
2023-07-04 15:52:02 +0700

    [TransactionItem] UI update

--- app/src/main/java/com/dcns/dailycost/foundation/uicomponent/TransactionItem.kt
+++ app/src/main/java/com/dcns/dailycost/foundation/uicomponent/TransactionItem.kt
@@ -1,11 +1,14 @@
 package com.dcns.dailycost.foundation.uicomponent
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -18,12 +21,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dcns.dailycost.R
 import com.dcns.dailycost.data.datasource.local.LocalExpenseDataProvider
 import com.dcns.dailycost.foundation.common.CommonDateFormatter
 import com.dcns.dailycost.foundation.common.LocalCurrency
@@ -58,10 +63,9 @@ fun TransactionItem(
                 )
         ) {
             Icon(
-                painter = ColorPainter(Color.LightGray),
+                painter = painterResource(id = transaction.category.icon.iconResId),
                 contentDescription = null,
                 modifier = Modifier
-                    .size(32.dp)
                     .clip(CircleShape)
             )
 
@@ -70,40 +74,81 @@ fun TransactionItem(
                 modifier = Modifier
                     .weight(1f)
             ) {
-                Text(
-                    text = transaction.name,
-                    style = MaterialTheme.typography.titleMedium.copy(
-                        fontWeight = FontWeight.Medium,
-                        color = DailyCostTheme.colorScheme.text
-                    ),
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
                     modifier = Modifier
-                        .dailyCostMarquee(
-                            edgeWidth = 0.dp
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = transaction.name,
+                        style = MaterialTheme.typography.titleMedium.copy(
+                            fontWeight = FontWeight.Medium,
+                            color = DailyCostTheme.colorScheme.text
+                        ),
+                        modifier = Modifier
+                            .weight(1f)
+                            .dailyCostMarquee(
+                                edgeWidth = 8.dp
+                            )
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .background(MaterialTheme.colorScheme.tertiaryContainer)
+                    ) {
+                        Text(
+                            text = stringResource(
+                                id = if (transaction.isExpense) R.string.expenses
+                                else R.string.income
+                            ),
+                            style = MaterialTheme.typography.bodyMedium.copy(
+                                color = MaterialTheme.colorScheme.tertiary
+                            ),
+                            modifier = Modifier
+                                .padding(
+                                    vertical = 4.dp,
+                                    horizontal = 8.dp
+                                )
                         )
-                )
+                    }
+                }
 
-                Text(
-                    text = CommonDateFormatter.edmy(context.resources.configuration.locales[0]).format(transaction.date),
-                    style = MaterialTheme.typography.labelSmall.copy(
-                        fontWeight = FontWeight.Normal,
-                        color = DailyCostTheme.colorScheme.labelText
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = CommonDateFormatter.edmy(context.resources.configuration.locales[0]).format(transaction.date),
+                        style = MaterialTheme.typography.labelSmall.copy(
+                            fontWeight = FontWeight.Normal,
+                            color = DailyCostTheme.colorScheme.labelText
+                        ),
+                        modifier = Modifier
+                            .weight(0.4f)
                     )
-                )
+
+                    Text(
+                        text = transaction.parseAmount(LocalCurrency.current.countryCode),
+                        textAlign = TextAlign.End,
+                        style = MaterialTheme.typography.bodyMedium.copy(
+                            fontWeight = FontWeight.SemiBold,
+                            color = DailyCostTheme.colorScheme.text
+                        ),
+                        modifier = Modifier
+                            .weight(0.6f)
+                            .dailyCostMarquee(
+                                edgeWidth = 0.dp
+                            )
+                    )
+                }
             }
-
-            Text(
-                text = transaction.parseAmount(LocalCurrency.current.countryCode),
-                textAlign = TextAlign.End,
-                style = MaterialTheme.typography.bodyMedium.copy(
-                    fontWeight = FontWeight.SemiBold,
-                    color = DailyCostTheme.colorScheme.text
-                ),
-                modifier = Modifier
-                    .weight(1f)
-                    .dailyCostMarquee(
-                        edgeWidth = 0.dp
-                    )
-            )
         }
     }
 }
