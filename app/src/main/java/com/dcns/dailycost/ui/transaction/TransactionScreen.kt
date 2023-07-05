57992c03449caf7e97821a1f4154076264cf73b9
kafri8889 <kafri8889@gmail.com>
2023-07-05 18:37:04 +0700

    UI update

--- app/src/main/java/com/dcns/dailycost/ui/transaction/TransactionScreen.kt
+++ app/src/main/java/com/dcns/dailycost/ui/transaction/TransactionScreen.kt
@@ -1,13 +1,18 @@
 package com.dcns.dailycost.ui.transaction
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.dcns.dailycost.R
 import com.dcns.dailycost.data.NavigationActions
@@ -16,22 +21,43 @@ import com.dcns.dailycost.foundation.base.BaseScreenWrapper
 @Composable
 fun TransactionScreen(
     viewModel: TransactionViewModel,
-    navigationActions: NavigationActions
+    navigationActions: NavigationActions,
 ) {
 
     val state by viewModel.state.collectAsStateWithLifecycle()
 
     BaseScreenWrapper(
         viewModel = viewModel,
-        topBar = {
+    ) { _ ->
+        ExpenseScreenContent(
+            state = state,
+            onNavigationIconClicked = {
+                navigationActions.popBackStack()
+            },
+            modifier = Modifier
+                .statusBarsPadding()
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ExpenseScreenContent(
+    state: TransactionState,
+    modifier: Modifier = Modifier,
+    onNavigationIconClicked: () -> Unit
+) {
+
+    LazyColumn(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+    ) {
+        item {
             TopAppBar(
                 title = {},
                 navigationIcon = {
-                    IconButton(
-                        onClick = {
-
-                        }
-                    ) {
+                    IconButton(onClick = onNavigationIconClicked) {
                         Icon(
                             painter = painterResource(id = R.drawable.ic_arrow_left),
                             contentDescription = null
@@ -40,17 +66,5 @@ fun TransactionScreen(
                 }
             )
         }
-    ) { scaffoldPadding ->
-        ExpenseScreenContent(
-            modifier = Modifier
-                .padding(scaffoldPadding)
-        )
     }
-}
-
-@Composable
-private fun ExpenseScreenContent(
-    modifier: Modifier = Modifier
-) {
-
 }
--- app/src/main/java/com/dcns/dailycost/ui/transactions/TransactionsScreen.kt
+++ app/src/main/java/com/dcns/dailycost/ui/transactions/TransactionsScreen.kt
@@ -3,12 +3,15 @@ package com.dcns.dailycost.ui.transactions
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.TopAppBar
@@ -21,7 +24,9 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.dcns.dailycost.R
 import com.dcns.dailycost.data.NavigationActions
+import com.dcns.dailycost.data.TopLevelDestinations
 import com.dcns.dailycost.foundation.base.BaseScreenWrapper
+import com.dcns.dailycost.foundation.theme.DailyCostTheme
 import com.dcns.dailycost.foundation.uicomponent.TransactionItem
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -35,15 +40,53 @@ fun TransactionsScreen(
 
     BaseScreenWrapper(
         viewModel = viewModel,
-        topBar = {
+        floatingActionButton = {
+            FloatingActionButton(
+                shape = CircleShape,
+                containerColor = DailyCostTheme.colorScheme.primary,
+                onClick = {
+                    navigationActions.navigateTo(
+                        destination = TopLevelDestinations.Home.transaction
+                    )
+                }
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_add),
+                    contentDescription = null,
+                    tint = DailyCostTheme.colorScheme.onPrimary
+                )
+            }
+        }
+    ) { _ ->
+        TransactionsScreenContent(
+            state = state,
+            onNavigationIconClicked = {
+                navigationActions.popBackStack()
+            },
+            modifier = Modifier
+                .statusBarsPadding()
+        )
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
+@Composable
+private fun TransactionsScreenContent(
+    state: TransactionsState,
+    modifier: Modifier = Modifier,
+    onNavigationIconClicked: () -> Unit
+) {
+
+    LazyColumn(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+    ) {
+        item {
             TopAppBar(
                 title = {},
                 navigationIcon = {
-                    IconButton(
-                        onClick = {
-
-                        }
-                    ) {
+                    IconButton(onClick = onNavigationIconClicked) {
                         Icon(
                             painter = painterResource(id = R.drawable.ic_arrow_left),
                             contentDescription = null
@@ -52,28 +95,7 @@ fun TransactionsScreen(
                 }
             )
         }
-    ) { scaffoldPadding ->
-        TransactionsScreenContent(
-            state = state,
-            modifier = Modifier
-                .padding(scaffoldPadding)
-                .fillMaxSize()
-        )
-    }
-}
 
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-private fun TransactionsScreenContent(
-    state: TransactionsState,
-    modifier: Modifier = Modifier
-) {
-
-    LazyColumn(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        modifier = modifier
-    ) {
         items(
             items = state.transactions,
             key = { item -> item.id }
@@ -87,6 +109,11 @@ private fun TransactionsScreenContent(
                     .fillMaxWidth(0.92f)
                     .animateItemPlacement(tween(256))
             )
+        }
+
+        item {
+            // Fab size: 56.dp
+            Spacer(modifier = Modifier.height(56.dp + 16.dp))
         }
     }
 }
