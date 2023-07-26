9ba5494edf968efd2782e2e9cb97521b36779c12
kafri8889 <kafri8889@gmail.com>
2023-07-26 12:55:18 +0700

    [worker] New worker `PostExpenseWorker` and add docs

--- app/src/main/java/com/dcns/dailycost/foundation/worker/Workers.kt
+++ app/src/main/java/com/dcns/dailycost/foundation/worker/Workers.kt
@@ -5,16 +5,27 @@ import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.workDataOf
+import com.dcns.dailycost.data.model.remote.request_body.AddExpenseRequestBody
 import com.dcns.dailycost.data.model.remote.request_body.DepoRequestBody
 import com.dcns.dailycost.data.model.remote.request_body.IncomeRequestBody
 
 object Workers {
 
+    /**
+     * Key untuk cek apakah sukses atau tidak
+     */
     const val ARG_WORKER_IS_SUCCESS_KEY = "worker_is_success"
+    /**
+     * Key untuk message
+     */
     const val ARG_WORKER_MESSAGE_KEY = "worker_message"
 
+    /**
+     * Key untuk request body
+     */
     const val ARG_DATA_REQUEST_BODY = "data_request_body"
 
+    const val TAG_POST_EXPENSE_BALANCE_WORKER = "worker_tag_post_expense_balance"
     const val TAG_POST_INCOME_BALANCE_WORKER = "worker_tag_post_income_balance"
     const val TAG_EDIT_BALANCE_WORKER = "worker_tag_edit_balance"
     const val TAG_SYNC_WORKER = "worker_tag_sync_balance"
@@ -32,6 +43,22 @@ object Workers {
                 )
             )
             .addTag(TAG_POST_INCOME_BALANCE_WORKER)
+            .build()
+    }
+
+    fun postExpenseWorker(body: AddExpenseRequestBody): OneTimeWorkRequest {
+        return OneTimeWorkRequestBuilder<PostExpenseWorker>()
+            .setConstraints(
+                Constraints(
+                    requiredNetworkType = NetworkType.CONNECTED
+                )
+            )
+            .setInputData(
+                workDataOf(
+                    ARG_DATA_REQUEST_BODY to body.toJson()
+                )
+            )
+            .addTag(TAG_POST_EXPENSE_BALANCE_WORKER)
             .build()
     }
 
