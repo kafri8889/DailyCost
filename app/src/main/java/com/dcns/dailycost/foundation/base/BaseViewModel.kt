9a70abc77da89d1b7509641864062f7f97580d3f
kafri8889 <kafri8889@gmail.com>
2023-09-19 09:35:38 +0700

    [BaseViewModel] Add docs

--- app/src/main/java/com/dcns/dailycost/foundation/base/BaseViewModel.kt
+++ app/src/main/java/com/dcns/dailycost/foundation/base/BaseViewModel.kt
@@ -4,14 +4,10 @@ import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 /**
@@ -29,8 +25,8 @@ abstract class BaseViewModel<STATE: Parcelable, ACTION>(
 	private val defaultState: STATE
 ): ViewModel() {
 
+	// Key yang digunakan untuk menyimpan dan mengambil state di savedStateHandle
 	private val KEY_STATE = "state"
-	private val typeToken = object : TypeToken<STATE>() {}
 
 	private val _uiEvent = Channel<UiEvent?>()
 	val uiEvent: Flow<UiEvent?> = _uiEvent.receiveAsFlow()
@@ -38,22 +34,20 @@ abstract class BaseViewModel<STATE: Parcelable, ACTION>(
 	private val _uiEventResult = Channel<UiEventResult?>()
 	val uiEventResult: Flow<UiEventResult?> = _uiEventResult.receiveAsFlow()
 
-	private val _state = MutableStateFlow(defaultState)
-	val state: StateFlow<STATE> = _state
-
-	init {
-		viewModelScope.launch {
-			savedStateHandle.getStateFlow(KEY_STATE, defaultState)
-				.collectLatest { state ->
-					_state.update { state }
-				}
-		}
-	}
+	// Key yang digunakan untuk menyimpan dan mengambil state di savedStateHandle
+	val state: StateFlow<STATE> = savedStateHandle.getStateFlow(KEY_STATE, defaultState)
 
 	abstract fun onAction(action: ACTION)
 
+	/**
+	 * Function yang digunakan untuk memperbarui state dari [savedStateHandle]
+	 *
+	 * @param newState parameter ini akan memberikan state saat ini sebagai `this`.
+	 */
 	protected fun updateState(newState: STATE.() -> STATE) {
+		// get current state
 		savedStateHandle.get<STATE>(KEY_STATE)?.let { state ->
+			// simpan state baru ke savedStateHandle
 			savedStateHandle[KEY_STATE] = newState(state)
 		}
 	}
